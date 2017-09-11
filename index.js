45776cc1f78699dc28e564163cbd503bc60438c0
fabioyo <fabio.ufheil@uconn.edu>
2017-09-11 14:53:15 -0400

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-var string = "Fuck You, Jeff Koons";
-var str = string.split("");
-var el = document.getElementById('str');
-(function animate() {
-str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
-var running = setTimeout(animate, 90);
-})();
